95e58f5f3d9c116bb33a956bb7250a24b26963b0
KhanxMo <khanm244@mcmaster.ca>
2023-06-23 12:50:51 -0400

    basic plus and equals

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// Script for Calculator 
 
 
+
+// Defining Variables
 let btns = document.querySelectorAll('.btn');
 let screenText = document.querySelector('#screenDisp')
 
@@ -8,8 +11,19 @@ let num2 = null;
 let opr = null;
 
 let dispTxt = "0"
+let clearNext = false;
 
+let lastKey = null;
+
+/**
+ * 
+ * @param {Number} num1 The first number in the operation. 
+ * @param {Number} num2 The second number in the operation. 
+ * @param {String} opr The type of operation (div, mul, min, pls).
+ * @returns {Number} The result of the operation. 
+ */
 function operate(num1, num2, opr){
+
     if (opr == "div"){
         return num1 / num2;
     }
@@ -25,53 +39,96 @@ function operate(num1, num2, opr){
 }
 
 
+
+function del(text){
+
+    if (text.length == 1){
+        return "0";
+    }
+    return text.slice(0, -1);
+}
+
+function dot(text){
+    // If dot does not exist, add a dot.
+    if (text.indexOf(".") == -1){
+        return text += "."
+    }
+    return text
+}
+
+
+
+
+
+// Called when a button is clicked, and handles what to do with the click. 
 function onBtnClick(event){
 
+    // Getting what is clicked
     let btnId = event.target.id
     console.log(btnId);
     
+    // Cases based on whats is clicked
     if (btnId == 'ac'){
         dispTxt = "0"
         num1 = 0;
         num2 = 0;
+        lastKey = "ac";
     }
     else if(btnId == 'del'){
-        dispTxt = "0"
+        dispTxt = del(dispTxt);
+        lastKey = "del";
     }
+    else if(btnId == 'dot'){
+        dispTxt = dot(dispTxt);
+        lastKey = "dot";
+    }
+
     else if(btnId == 'div'){
         opr = 'div';
+        lastKey = "div";
     }
     else if(btnId == 'mul'){
         opr = 'mul';
+        lastKey = "mul";
     }
     else if(btnId == 'min'){
         opr = 'min';
+        lastKey = "min";
     }
     else if(btnId == 'pls'){
-        opr = 'pls';
-
-        if (num1 == null){
-            num1 = Number(dispTxt);
-            dispTxt = "0";
-        } else{
-            num2 = Number(dispTxt);
-            num1 = operate(num1, num2, opr);
-            num2 = null;
-            dispTxt = String
+        if (lastKey == "num"){
+            opr = 'pls';
+            if (num1 == null){
+                num1 = Number(dispTxt);
+                dispTxt = "0";
+            }
+            else{
+                num1 = operate(num1, Number(dispTxt), opr);
+                dispTxt = num1;
+                clearNext = true;
+            }
         }
-
+        lastKey = "pls";
     }
     else if(btnId == 'eql'){
-        opr = 'eql';
-    }
-    else if(btnId == 'dot'){
-        if (dispTxt.indexOf(".") == -1){
-            dispTxt += "."
+        
+        if (lastKey == "num"){
+            if (num1){
+                num1 = operate(num1, Number(dispTxt), opr);
+                dispTxt = num1;
+                clearNext = true;
+            }
         }
+
+        lastKey = "eql";
     }
     
     // If a number is pressed
     else {
+
+        if (clearNext){
+            dispTxt = "0";
+        }
 
         if (dispTxt === "0"){
             dispTxt = btnId;
@@ -79,13 +136,16 @@ function onBtnClick(event){
         else {
             dispTxt += btnId;
         }
-        
+
+        lastKey = "num";
     }
 
     // Update Screen Text
     screenText.textContent = dispTxt;
 
 }
+
+
 
 btns.forEach((btn) => {
     btn.addEventListener('click', onBtnClick)
